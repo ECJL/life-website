53e87b411a04b6e0a00ee0f085e873de2ce36bda
df1 <whsh320@gmail.com>
2017-07-17 23:53:40 +0800

    remove react bootstrap

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import {Col} from 'react-bootstrap';
 
 export default class Header extends React.Component{
     render(){
         return (
             <div>
-                <Col xs={12} md={3}>
+                <div className="col-md-3">
                     {this.props.children}
-                </Col>
-                <Col xs={12} md={9}>
+                </div>
+                <div className="col-md-9">
                     <nav>
                         <ul>
                             <li>Home</li>
@@ -18,7 +17,7 @@ export default class Header extends React.Component{
                             <li>Contact</li> {/* donate & sponsor*/}
                         </ul>
                     </nav>
-                </Col>
+                </div>
             </div>
         );
     }
