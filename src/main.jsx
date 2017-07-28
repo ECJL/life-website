15446be25d038270a0f73ae6f40c5b4e615e08fa
vacatehoping <vacatehoping@gmail.com>
2017-07-28 23:18:29 +0800

    add Section

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import {navItems} from './utils/constants'
 import {render} from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import Header from './components/header/header.jsx'
+import Section from './components/section/section.jsx'
 import Footer from './components/footer/footer.jsx'
 
 
@@ -13,6 +14,11 @@ class RootComponent extends React.Component {
 		return (
 			<div>
                 <Header navItems={navItems}>Ellis &amp; Jen</Header>
+                <Section sectionName='Home'></Section>
+                <Section sectionName='About'></Section>
+                <Section sectionName='Life'></Section>
+                <Section sectionName='Work'></Section>
+                <Section sectionName='Contact'></Section>
 				<Footer/>
 			</div>
 		);
