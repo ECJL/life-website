744a5a2dc4bac4f056ad3581b316f8058732f05a
df1 <whsh320@gmail.com>
2017-07-28 23:29:10 +0800

    let footer show current year

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -3,6 +3,7 @@ import styles from './footer.css';
 
 export default class Footer extends React.Component{
 	render(){
-		return (<div className={styles.footer}>Copyright &copy; 2017 Ellis &amp; Jen | All rights reserved</div>);
+        let currentYear = new Date().getFullYear();
+		return (<div className={styles.footer}>Copyright &copy; {currentYear} Ellis &amp; Jen | All rights reserved</div>);
 	}
 }
