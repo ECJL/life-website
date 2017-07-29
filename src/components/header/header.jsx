2f507510cdb0609b5437e12c0f494d4d00bcc4c9
vacatehoping <vacatehoping@gmail.com>
2017-07-29 15:11:35 +0800

    add section & header css

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import styles from './header.css';
 
 export default class Header extends React.Component{
     render(){
         return (
             <div className="row">
-                <div className="col-md-3">
+                <div className={`col-md-3 ${styles.hero}`}>
                     {this.props.children}
                 </div>
                 <div className="col-md-9">
-                    <nav className="navbar pull-right">
+                    <nav className={`navbar pull-right ${styles.navBar}`}>
                         <ul className="nav navbar-nav">
                             {this.props.navItems.map( item =>
                                 <li key={item.name}>
--- src/components/section/section.jsx
+++ src/components/section/section.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import styles from './section.css';
 
 export default class Section extends React.Component{
     render(){
         return (
             <div className="row">
-                <div className="col-md-12">
+                <div className={`col-md-12 ${styles.section}`}>
                     {this.props.sectionName}
                 </div>
             </div>
