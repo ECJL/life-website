60b308d92a77c7ba5d3fc5f5e14f55dc8e614f2a
vacatehoping <vacatehoping@gmail.com>
2017-07-31 23:39:12 +0800

    add classnames

--- src/components/header/nav-item.jsx
+++ src/components/header/nav-item.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import classNames from 'classnames';
 
 export default class NavItem extends React.Component{
 	render(){
+    	var navItemLiClasses = classNames(
+    		{active:this.props.active}
+    	);
+
         return (
-        	<li>
+        	<li className={navItemLiClasses}>
                 <a onClick={this.props.onClick} href={`#${this.props.itemName}`}>
                     {this.props.itemName} {this.props.active?'*':''}</a>
             </li>
